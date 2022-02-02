af30a272fff45427ac739d75dde0a175f65606b2
Евгений <54ksenon@gmail.com>
2022-02-03 00:31:40 +0600

    перенёс цикл pygame в main, прописал названия кнопок в меню, перенес отрисовку меню в Manager.draw, выбрал шрифт надписей кнопок

--- seabattlemain.py
+++ seabattlemain.py
@@ -4,10 +4,11 @@ import pygame
 
 
 class Menu:
-    def __init__(self):
-        image = pygame.image.load('seabattlemenu.jpg')
-        image_rect = image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        screen.blit(image, image_rect)
+    def __init__(self, screen):
+        self.image = pygame.image.load('sea_battle_menu.jpg')
+        self.text = pygame.font.Font('C:\\Windows\\Fonts\\segoepr.ttf', SHRIFT_SIZE)
+        self.buttons = ['Играть одному!', 'Играть с другом!', 'Рекорды', 'Справка']
+        self.screen = screen
 
 
 class Game:
@@ -15,7 +16,15 @@ class Game:
 
 
 class Manager:
-    pass
+    def __init__(self, screen):
+        self.menu = Menu(screen)
+
+    def draw(self):
+        image_rect = self.menu.image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        self.menu.screen.blit(self.menu.image, image_rect)
+        text_render = self.menu.text.render('Играть одному!', True, 'black')
+        text_rect = text_render.get_rect(center=(WIDTH*0.4, HEIGHT // 3))
+        self.menu.screen.blit(text_render, text_rect)
 
 
 class Round:
@@ -51,21 +60,23 @@ class Draw:
 
 
 def main():
-    pass
-
-
-if __name__ == '__main__':
     pygame.display.init()
+    pygame.font.init()
     pygame.display.set_caption('Sea Battle')
     screen = pygame.display.set_mode(SCREEN_SIZE)
+    manager = Manager(screen)
     finished = False
     clock = pygame.time.Clock()
-    menu = Menu()
     while not finished:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
+        manager.draw()
         pygame.display.update()
 
     pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
